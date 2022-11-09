42220096153131de6bb8b37840d44e5cfe5fa026
ps170116 <ps170116@edu.summacollege.nl>
2022-11-09 22:41:00 +0100

    Added Interact System

---
+++ Assets/Scripts/Player/Interact.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Interact : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI itemText;
+    [SerializeField] float interactRange;
+    [HideInInspector] public GameObject currentItem;
+    public LayerMask itemLayer;
+    [SerializeField] Camera cam;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, interactRange, itemLayer))
+        {
+            if (hit.collider.gameObject.GetComponent<Interactable>() != null)
+            {
+                Interactable interactable = hit.collider.gameObject.GetComponent<Interactable>();
+                itemText.text = interactable.interactableName;
+                itemText.gameObject.SetActive(true);
+                if (Input.GetButton("Interact"))
+                {
+                    interactable.Interact();
+                }
+            }
+        }
+        else
+        {
+            itemText.gameObject.SetActive(false);
+        }
+    }
+
+
+}
